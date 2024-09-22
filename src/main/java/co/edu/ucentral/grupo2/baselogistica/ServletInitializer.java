0260a29bca419a3036389cebbfe4e0dc4f332ab2
LinMichRa <lramirezd2@ucentral.edu.co>
2024-09-22 16:28:26 -0500

    Proyecto creado y base de datos configurada

---
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.grupo2.baselogistica;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BaselogisticaBackendApplication.class);
+	}
+
+}
