67878afe8d820d8736203752723fff358e2fc6e3
LinMichRa <lramirezd2@ucentral.edu.co>
2024-09-25 23:10:21 -0500

    Arreglo servicio .save vehiculo

--- src/main/java/co/edu/ucentral/grupo2/baselogistica/repositorios/RepositoriosVehiculos.java
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/repositorios/RepositoriosVehiculos.java
@@ -1,10 +1,11 @@
 package co.edu.ucentral.grupo2.baselogistica.repositorios;
 
-import co.edu.ucentral.grupo2.baselogistica.modelos.vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import co.edu.ucentral.grupo2.baselogistica.modelos.vehiculo;
+
 @Repository
-public interface RepositoriosVehiculos extends  JpaRepository <vehiculo,Integer > {
+public interface RepositoriosVehiculos extends  JpaRepository <vehiculo,Integer> {
 
 }
--- src/main/java/co/edu/ucentral/grupo2/baselogistica/servicios/Vehiculos.java
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/servicios/Vehiculos.java
@@ -1,15 +1,19 @@
 package co.edu.ucentral.grupo2.baselogistica.servicios;
 
-import co.edu.ucentral.grupo2.baselogistica.modelos.vehiculo;
-import co.edu.ucentral.grupo2.baselogistica.repositorios.RepositoriosVehiculos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.ucentral.grupo2.baselogistica.modelos.vehiculo;
+import co.edu.ucentral.grupo2.baselogistica.repositorios.RepositoriosVehiculos;
+
 @Service
-public class Vehiculos {
-@Autowired
-    RepositoriosVehiculos repositoriosVehiculos;
-@Override
-    public vehiculo crear(vehiculo vehiculo){return repositoriosVehiculos.save(vehiculo); }
+public class Vehiculos{
+
+    @Autowired
+    private RepositoriosVehiculos repositoriosVehiculos;
+
+    public vehiculo guardarVehiculo(vehiculo vehiculo){
+        return repositoriosVehiculos.save(vehiculo);
+    }
 
 }
