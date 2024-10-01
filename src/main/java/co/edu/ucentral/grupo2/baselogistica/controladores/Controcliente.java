ef33aef92690316668539242563acdc3cfd324bb
LinMichRa <lramirezd2@ucentral.edu.co>
2024-09-30 21:08:51 -0500

    Finalización rutas guardar cliente, conductor, despachador y pedido

--- src/main/java/co/edu/ucentral/grupo2/baselogistica/controladores/Controcliente.java
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/controladores/Controcliente.java
@@ -1,5 +1,27 @@
 package co.edu.ucentral.grupo2.baselogistica.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ucentral.grupo2.baselogistica.modelos.cliente;
+import co.edu.ucentral.grupo2.baselogistica.servicios.SerCliente;
+
+
+@RestController
+@RequestMapping("/api/cliente")
 public class Controcliente {
+    @Autowired
+    private SerCliente clienteServicio;
+
+    @PostMapping("/registrarCliente")
+    public ResponseEntity<cliente> guardarCliente(@ModelAttribute cliente cliente){
+        cliente clienteGuardado = clienteServicio.guardarCliente(cliente);
+        return new ResponseEntity<>(clienteGuardado, HttpStatus.CREATED);
+    }
     
 }
--- src/main/java/co/edu/ucentral/grupo2/baselogistica/controladores/Contropedido.java
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/controladores/Contropedido.java
@@ -1,5 +1,27 @@
 package co.edu.ucentral.grupo2.baselogistica.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ucentral.grupo2.baselogistica.modelos.pedido;
+import co.edu.ucentral.grupo2.baselogistica.servicios.SerPedidos;
+
+
+@RestController
+@RequestMapping("api/pedidos")
 public class Contropedido {
-    
+    @Autowired
+    private SerPedidos pedidosServicio;
+
+    //Defincion enrutamiento registrar pedido
+    @PostMapping("/registroPedido")  
+    public ResponseEntity<pedido> guardarPedido (@ModelAttribute pedido pedido){
+        pedido pedidoGuardado = pedidosServicio.guardaPedido(pedido);
+        return new ResponseEntity<>(pedidoGuardado, HttpStatus.CREATED);
+    }
 }
