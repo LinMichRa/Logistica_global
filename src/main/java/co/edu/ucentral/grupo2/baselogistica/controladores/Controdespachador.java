2301b010e67f05cdea8575c209754ffb02e79335
LinMichRa <lramirezd2@ucentral.edu.co>
2024-10-05 16:06:02 -0500

    arreglo conflictos controlador

--- src/main/java/co/edu/ucentral/grupo2/baselogistica/controladores/Controdespachador.java
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/controladores/Controdespachador.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +26,10 @@ public class Controdespachador {
         return new ResponseEntity<>(despachadorGuardado, HttpStatus.CREATED);
     }
     
+    @PostMapping("/modificarDespachador/{cedula}")
+    public ResponseEntity<despachador>modificarDespachador(@PathVariable("cedula") Long cedula, @ModelAttribute despachador despachador){
+        despachador despachadorModificado = despachadorServicio.modificarDespachador(despachador);
+
+        return new ResponseEntity<>(despachadorModificado, HttpStatus.OK);
+    }
 }
