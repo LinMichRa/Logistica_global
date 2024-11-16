309f76c6e08b611f06285ddb6f8ef3ce6626b935
LinMichRa <lramirezd2@ucentral.edu.co>
2024-11-15 23:27:35 -0500

    Configuracion inyeccion y jwtAuthFilter en ApplicationConfig.java

--- src/main/java/co/edu/ucentral/grupo2/baselogistica/Config/ApplicationConfig.java
+++ src/main/java/co/edu/ucentral/grupo2/baselogistica/Config/ApplicationConfig.java
@@ -1,100 +1,28 @@
 package co.edu.ucentral.grupo2.baselogistica.Config;
 
-import java.io.IOException;
-import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import co.edu.ucentral.grupo2.baselogistica.exception.UnauthorizedException;
+import co.edu.ucentral.grupo2.baselogistica.security.JwtAuthFilter;
 import co.edu.ucentral.grupo2.baselogistica.security.JwtAuthenticationProvider;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ApplicationConfig extends OncePerRequestFilter{
+@Configuration
+public class ApplicationConfig{
 
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
 
-    /**
-     * Lista blanca de URIs
-     */
-    private List<String> urlsToSkip = List.of("/auth", "/swagger-ui.html", "/swagger-ui", "/api-docs");
-
-
-    /**
-     * Verifica si a la URI no se le debe aplicar el filtro
-     * @param request current HTTP request Petición a validar
-     * @return True la URI existe en la lista blanca, false de lo contrario
-     * @throws ServletException
-     */
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        System.out.println("en esta peticion se rompe");
-        System.out.println(request.getRequestURI());
-        return urlsToSkip.stream().anyMatch(url -> request.getRequestURI().contains(url));
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
-    /**
-     * Valida si la petición contiene la cabezera de authorization con el bearer token
-     * @param request
-     * @param response
-     * @param filterChain
-     * @throws ServletException
-     * @throws IOException
-     * @throws UnauthorizedException - Si no tiene la cabezera HttpHeaders.AUTHORIZATION
-     *                               - Si tiene más de dos elementos en al cabezera o no tiene 'Bearer'
-     *                               - Si el token no es valido
-     */
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-        /*
-        System.out.println("headers");
-        System.out.println(header);
-        System.out.println(request);
-
-
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            String headerValue = request.getHeader(headerName);
-            System.out.println("header and value");
-            System.out.println(headerName);
-            System.out.println(headerValue);
-            }
-          */
-          if (header == null) {
-            throw new UnauthorizedException();
-        }
-
-        String[] authElements = header.split(" ");
-
-        if (authElements.length != 2 || !"Bearer".equals(authElements[0])) {
-            throw new UnauthorizedException();
-        }
-
-        try {
-            Authentication auth = jwtAuthenticationProvider.validateToken(authElements[1]);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-
-            System.out.println("voy a imprimir el context");
-            System.out.println(SecurityContextHolder.getContext());
-            System.out.println("voy a imprimir la autenticacion");
-            System.out.println(SecurityContextHolder.getContext().getAuthentication());
-        } catch (RuntimeException e) {
-            SecurityContextHolder.clearContext();
-            System.out.println("se estalló");
-            System.out.println(e);
-            throw new RuntimeException(e);
-        }
-        System.out.println("llegué aqui");
-
-        filterChain.doFilter(request, response);
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(){
+        return new JwtAuthFilter(jwtAuthenticationProvider);
     }
+    
 }
